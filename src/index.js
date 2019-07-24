06fc30ea4e1a9fb983ec6371364f54cfd2f6f54c
nickolagr <mgrechanyk@rambler.ru>
2019-07-24 22:01:34 +0300

    changed init state

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import {createStore} from 'redux';
 import studentReducer from './reducers/studentReducer';
 import {Provider} from 'react-redux'
 
-let initialState = [{id:1,name:'John Doe',grade:1,school:'React Redux School'},{id:2,name:'Jane Doe',grade:2,school:'React Redux School'}
-,{id:3,name:'Terry Adams',grade:3,school:'React Redux School'},{id:4,name:'Jenny Smith',grade:4,school:'React Redux School'}];
+let initialState = [{id:1,name:'Hot Dog po vinnitski',grade:1,school:'React Redux School'},{id:2,name:'Hot Dog with meat',grade:2,school:'React Redux School'}
+,{id:3,name:'Vegan\'s Hot Dog',grade:3,school:'React Redux School'},{id:4,name:'Hot Dog with pork',grade:4,school:'React Redux School'}];
 
 if( localStorage.getItem("students") === null)
 localStorage.setItem('students',JSON.stringify(initialState));
